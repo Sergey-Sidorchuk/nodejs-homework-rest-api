4ee0589949b5efabefc1e9ce7e04fc762a9116a9
SergeySidorchuk <serg_ss@ukr.net>
2021-10-23 18:12:48 +0300

    fix hw4(current.js )

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,42 +1,20 @@
 const { User } = require("..//..//schemas");
+const { Unauthorized } = require("http-errors");
 
-const current = async (req, res, next) => {
-  try {
-    const userToken = req.user.token;
-    const user = await User.findUserByToken(userToken);
-
-    if (!user) {
-      throw new Unauthorized("Not authorized");
-    }
-
-    return res.status(200).json({
-      status: "Success",
-      code: 200,
-      data: {
-        email: user.email,
-        subscription: user.subscription,
-      },
-    });
-  } catch (error) {
-    next(error);
+const current = async (req, res) => {
+  const { _id } = req.user;
+  const user = await User.findById(_id, "email subscription");
+  if (!user) {
+    throw new Unauthorized("Not authorized");
   }
+
+  res.status(200).json({
+    status: "Success",
+    code: 200,
+    data: {
+      user,
+    },
+  });
 };
 
 module.exports = current;
-
-// const { User } = require('../../schemas')
-
-// const current = async (req, res, _) => {
-//   const [{ _id: id, email, subscription }] = await User.find(req.user)
-//   res.json({
-//     status: 'success',
-//     code: 200,
-//     data: {
-//       id,
-//       email,
-//       subscription
-//     }
-//   })
-// }
-
-// module.exports = current
